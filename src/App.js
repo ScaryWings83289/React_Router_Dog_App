6d4c8eb6e345ed896495cb385791790a0edf737b
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2020-07-24 10:09:43 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Navbar from "./Navbar";
 import whiskey from "./imgs/whiskey.jpg";
 import tubby from "./imgs/tubby.jpg";
 import hazel from "./imgs/hazel.jpg";
+import charlie from "./imgs/charlie.jpg";
+import mike from "./imgs/mike.jpg";
+import laura from "./imgs/laura.jpg";
 import './App.css';
 
 class App extends Component {
@@ -37,6 +40,36 @@ class App extends Component {
           "Tubby is not the brightest dog",
           "Tubby does not like walks or exercise.",
           "Tubby loves eating food."
+        ]
+      },
+      {
+        name: "Charlie",
+        age: 6,
+        src: charlie,
+        facts: [
+          "Charlie loves eating popcorn.",
+          "Charlie is a terrible guard dog.",
+          "Charlie wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Mike",
+        age: 5,
+        src: mike,
+        facts: [
+          "Mike has soooo much energy!",
+          "Mike is highly intelligent.",
+          "Mike loves people more than dogs."
+        ]
+      },
+      {
+        name: "Laura",
+        age: 8,
+        src: laura,
+        facts: [
+          "Laura loves eating popcorn.",
+          "Laura is a terrible guard dog.",
+          "Laura wants to cuddle with you!"
         ]
       }
     ]
